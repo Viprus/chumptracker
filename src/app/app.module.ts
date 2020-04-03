30906cd022cf9612369c85951d1e349a5684bd14
Viprus <viprus@hotmail.co.uk>
2020-04-03 23:01:29 +0100

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,10 +27,3 @@ import { ChumpDetailComponent } from './chump-detail/chump-detail.component';
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
